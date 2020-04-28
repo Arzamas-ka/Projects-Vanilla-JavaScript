d8e45bcdc917e1d233b57071548ac0e7036b74e2
Liudmila_Arzamasova <liudmila.arzamasova@gmail.com>
2020-04-28 21:11:00 +0300

    feat: implement sortable list project

--- sortable-list/script.js
+++ sortable-list/script.js
@@ -1,17 +1,17 @@
 const draggable_list = document.getElementById('draggable-list');
 const check = document.getElementById('check');
 
-const richestPeople = [
-  'Jeff Bezos',
-  'Bill Gates',
-  'Warren Buffett',
-  'Bernard Arnault',
-  'Carlos Slim Helu',
-  'Amancio Ortega',
-  'Larry Ellison',
-  'Mark Zuckerberg',
-  'Michael Bloomberg',
-  'Larry Page'
+const listOfFoods = [
+  'Rolls',
+  'Pizza',
+  'Pasta',
+  'Fish',
+  'Potatoes',
+  'Cherry',
+  'Lemon',
+  'Milk',
+  'Bread',
+  'Strawberry',
 ];
 
 // Store listitems
@@ -19,19 +19,15 @@ const listItems = [];
 
 let dragStartIndex;
 
-createList();
-
 // Insert list items into DOM
-function createList() {
-  [...richestPeople]
-    .map(a => ({ value: a, sort: Math.random() }))
+const createList = () => {
+  [...listOfFoods]
+    .map((name) => ({ value: name, sort: Math.random() }))
     .sort((a, b) => a.sort - b.sort)
-    .map(a => a.value)
+    .map((name) => name.value)
     .forEach((person, index) => {
       const listItem = document.createElement('li');
-
       listItem.setAttribute('data-index', index);
-
       listItem.innerHTML = `
         <span class="number">${index + 1}</span>
         <div class="draggable" draggable="true">
@@ -41,78 +37,78 @@ function createList() {
       `;
 
       listItems.push(listItem);
-
       draggable_list.appendChild(listItem);
     });
-
-  addEventListeners();
-}
+};
+createList();
 
 function dragStart() {
-  // console.log('Event: ', 'dragstart');
   dragStartIndex = +this.closest('li').getAttribute('data-index');
+  console.log(dragStartIndex);
 }
 
 function dragEnter() {
-  // console.log('Event: ', 'dragenter');
   this.classList.add('over');
 }
 
-function dragLeave() {
-  // console.log('Event: ', 'dragleave');
-  this.classList.remove('over');
-}
-
-function dragOver(e) {
-  // console.log('Event: ', 'dragover');
-  e.preventDefault();
+function dragOver(event) {
+  this.classList.add('over');
+  event.preventDefault();
 }
 
 function dragDrop() {
-  // console.log('Event: ', 'drop');
   const dragEndIndex = +this.getAttribute('data-index');
   swapItems(dragStartIndex, dragEndIndex);
 
   this.classList.remove('over');
 }
 
+function dragLeave() {
+  this.classList.add('over');
+}
+
 // Swap list items that are drag and drop
-function swapItems(fromIndex, toIndex) {
+const swapItems = (fromIndex, toIndex) => {
+  // console.log(fromIndex);
+  // console.log(toIndex);
   const itemOne = listItems[fromIndex].querySelector('.draggable');
   const itemTwo = listItems[toIndex].querySelector('.draggable');
 
   listItems[fromIndex].appendChild(itemTwo);
   listItems[toIndex].appendChild(itemOne);
-}
+};
 
-// Check the order of list items
-function checkOrder() {
+// Check the order of list items on button click
+const checkOrder = () => {
   listItems.forEach((listItem, index) => {
     const personName = listItem.querySelector('.draggable').innerText.trim();
 
-    if (personName !== richestPeople[index]) {
+    if (personName !== listOfFoods[index]) {
+      // console.log('personName ', personName);
+      // console.log(listOfFoods[index]);
       listItem.classList.add('wrong');
     } else {
       listItem.classList.remove('wrong');
       listItem.classList.add('right');
     }
   });
-}
+};
 
-function addEventListeners() {
+const addEventListeners = () => {
   const draggables = document.querySelectorAll('.draggable');
   const dragListItems = document.querySelectorAll('.draggable-list li');
 
-  draggables.forEach(draggable => {
+  draggables.forEach((draggable) => {
     draggable.addEventListener('dragstart', dragStart);
   });
 
-  dragListItems.forEach(item => {
+  dragListItems.forEach((item) => {
     item.addEventListener('dragover', dragOver);
     item.addEventListener('drop', dragDrop);
     item.addEventListener('dragenter', dragEnter);
     item.addEventListener('dragleave', dragLeave);
   });
-}
+};
+addEventListeners();
 
 check.addEventListener('click', checkOrder);
